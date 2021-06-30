178555397443c72a5a94e44bdd629ed56c4a1581
Maxi Isea <max.nirvana22@hotmail.com>
2021-06-29 21:57:05 -0300

    Problemas al almacenar datos en act4

--- app/src/main/java/com/example/ppc_tp3_isea/Screen4Activity.java
+++ app/src/main/java/com/example/ppc_tp3_isea/Screen4Activity.java
@@ -2,14 +2,21 @@ package com.example.ppc_tp3_isea;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.InputFilter;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Screen4Activity extends AppCompatActivity {
 
@@ -39,6 +46,8 @@ public class Screen4Activity extends AppCompatActivity {
 
     public void calculateRisk(View view){
 
+        String[] archivos = fileList();
+
         if (rad1.isChecked()){
             selected_radiob = true;
         } else if (rad2.isChecked()){
@@ -50,8 +59,50 @@ public class Screen4Activity extends AppCompatActivity {
         extras.putDouble("riesgoRec", Double.parseDouble(ed3.getText().toString()));
         extras.putBoolean("esquema", rad1.isChecked());
 
+        checkTexts(archivos, ed1);
+        checkTexts(archivos, ed2);
+        checkTexts(archivos, ed3);
+        Guardar(view);
+
         Intent goact5 = new Intent(this, Screen5Activity.class);
         goact5.putExtras(extras);
         startActivity(goact5);
+    }
+
+    public void checkTexts(String archivos[], EditText aux){
+        if(ArchivoExiste(archivos, "datos.txt")){
+            try {
+                InputStreamReader archivo = new InputStreamReader(openFileInput("datos.txt"));
+                BufferedReader br = new BufferedReader(archivo);
+                String linea = br.readLine();
+                aux.setText(linea);
+                br.close();
+                archivo.close();
+            }catch (IOException e){
+
+            }
+        }
+    }
+
+    private boolean ArchivoExiste(String archivos[], String NombreArchivo){
+        for (String archivo : archivos)
+            if (NombreArchivo.equals(archivo))
+                return true;
+        return false;
+    }
+
+    //Método para el botón Guardar
+    public void Guardar(View view){
+        try {
+                OutputStreamWriter archivo = new OutputStreamWriter(openFileOutput("datos.txt", Activity.MODE_PRIVATE));
+                archivo.write(ed1.getText().toString());
+                archivo.write(ed2.getText().toString());
+                archivo.write(ed3.getText().toString());
+                archivo.flush();
+                archivo.close();
+        }catch (IOException e){
+
+        }
+        Toast.makeText(this, "Datos almacenados correctamente", Toast.LENGTH_SHORT).show();
     }
 }
